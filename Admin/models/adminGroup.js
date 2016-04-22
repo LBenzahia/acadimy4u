fcae92f63b53449b86a3934e17cf272fba43b8c5
LBenzahia <lakhdar.benzahia@gmail.com>
2016-04-22 22:34:08 +0100

    fix some logical bugs

--- Admin/models/adminGroup.js
+++ Admin/models/adminGroup.js
@@ -1,12 +1,20 @@
-
+// admin/models/AdminGroup.js
+// load the things we need 
 var mongoose = require('mongoose');
 var relationship = require('mongoose-relationship');
+var User = require('../app_auth/models/user.js');
 var schema = mongoose.schema,
     ObjectId = schema.ObjectId;
   var adminGroupSchema = mongoose.Schema({
-    _id: { type: String },
+    _idOwner : { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
     name: { type: String, default: '' },
-    permissions: [{ name: String, permit: Boolean }]
+    permissions: {
+       name: String,
+       permit: Boolean
+      }
 
 
 });
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('AdminGroup', userSchema);
